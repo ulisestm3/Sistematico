6b5aa3c72d7b6f0336d7bc68c24cad5ce8a0d615
ulisesZuniga <ulisestm3@gmail.com>
2024-02-17 09:08:58 -0600

    update listas

--- Sesion05/Ejemplo3.py
+++ Sesion05/Ejemplo3.py
@@ -1,3 +1,13 @@
 #Listas
 lista = [1, 2, 3, "Manzana", 15.6, True]
 print(lista)
+
+#Imprimir el primer elemento de la lista
+print(lista[0])
+
+#Tamaño de la lista
+tam = len(lista)
+print(f"El tamaño es {tam}")
+
+#imprimir el ultimo valor
+print(lista[tam-1])
