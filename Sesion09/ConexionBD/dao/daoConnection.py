675147cfbd9da298708c7ca63377950fe1b92746
ulisesZuniga <ulisestm3@gmail.com>
2024-04-09 09:49:01 -0600

    [Update] CRUD Employee - Jobs

--- Sesion09/ConexionBD/dao/daoConnection.py
+++ Sesion09/ConexionBD/dao/daoConnection.py
@@ -41,16 +41,70 @@ class DaoCity:
         query = 'SELECT * FROM cities WHERE id = %s'
         return self.connection.execute_read_query(query, (id,))
     
+    def get_by_name(self, name):
+        query = 'SELECT * FROM cities WHERE name = %s'
+        return self.connection.execute_read_query(query, (name))
+    
     def insert(self, city):
         query = 'INSERT INTO cities (name, status) VALUES (%s, %s)'
         return self.connection.execute_query(query, (city.name, city.status))
     
-    def update(self, city):
+    def update(self, name, status, id):
         query = 'UPDATE cities SET name = %s, status = %s WHERE id = %s'
-        return self.connection.execute_query(query, (city.name, city.status, city.id))
+        return self.connection.execute_query(query, (name, status, id))
     
     def delete(self, id):
         query = 'DELETE FROM cities WHERE id = %s'
         return self.connection.execute_query(query, (id,))
+
+class DaoEmployees:
+    def __init__(self, connection):
+        self.connection = connection
     
+    def get_all(self):
+        query = 'SELECT * FROM employees'
+        return self.connection.execute_read_query(query, ())
     
+    def get_by_id(self, id):
+        query = 'SELECT * FROM employees WHERE id = %s'
+        return self.connection.execute_read_query(query, (id,))
+    '''
+    def insert(self, city):
+        query = 'INSERT INTO cities (name, status) VALUES (%s, %s)'
+        return self.connection.execute_query(query, (city.name, city.status))
+    '''
+    def insert(self, Employee):
+        query = 'INSERT INTO employees (nombre, apellido, ciudad_id, job_id, salary, status) VALUES (%s, %s, %s, %s, %s, %s)'
+        return self.connection.execute_query(query, (Employee.first_name, Employee.last_name, Employee.city, Employee.job, Employee.salary,Employee.status))
+    
+    def update(self, Employee, id):
+        query = 'UPDATE employees SET nombre = %s, apellido = %s, ciudad_id = %s, job_id = %s, salary = %s, status = %s WHERE id = %s'
+        return self.connection.execute_query(query, (Employee.first_name, Employee.last_name, Employee.city, Employee.job, Employee.salary,Employee.status, id))
+    
+    def delete(self, id):
+        query = 'DELETE FROM employees WHERE id = %s'
+        return self.connection.execute_query(query, (id,))
+    
+class DaoJobs:
+    def __init__(self, connection):
+        self.connection = connection
+    
+    def get_all(self):
+        query = 'SELECT * FROM jobs'
+        return self.connection.execute_read_query(query, ())
+    
+    def get_by_id(self, id):
+        query = 'SELECT * FROM jobs WHERE id = %s'
+        return self.connection.execute_read_query(query, (id,))
+    
+    def insert(self, job):
+        query = 'INSERT INTO jobs (name, status) VALUES (%s, %s)'
+        return self.connection.execute_query(query, (job.name, job.status))
+    
+    def update(self, Job, id):
+        query = 'UPDATE jobs SET name = %s, status = %s WHERE id = %s'
+        return self.connection.execute_query(query, (Job.name, Job.status, id))
+    
+    def delete(self, id):
+        query = 'DELETE FROM jobs WHERE id = %s'
+        return self.connection.execute_query(query, (id,))
--- Sesion09/ConexionBD/main.py
+++ Sesion09/ConexionBD/main.py
@@ -10,25 +10,311 @@ conex.connect()
 #instanciar dao
 daoCity = dao.DaoCity(conex)
 
-'''
-#instanciar modelo
-city1=clases.City("Managua",1)
-city2=clases.City("Leon",1)
-city3=clases.City("Granada",1)
-city4=clases.City("Masaya",1)
-city5=clases.City("Estelí",1)
-city6=clases.City("Jinotepe",1)
+#CRUD Ciudad
+def insertarCiudad():
+    nombre = input("Nombre ciudad: ")
 
-#insertar
-daoCity.insert(city1)
-daoCity.insert(city2)
-daoCity.insert(city3)
-daoCity.insert(city4)
-daoCity.insert(city5)
-daoCity.insert(city6)
+    if nombre!="" and nombre !=" ":
+        ciudad = clases.City(nombre,1)
+        daoCity = dao.DaoCity(conex)
+        #insertar
+        daoCity.insert(ciudad)
 
-'''
-#Consultar
-cities = daoCity.get_all()
-for city in cities:
-    print(city)
+    else:
+        os.system("cls")
+        print("Favor digitar el nombre de la ciudad")
+        insertarCiudad()
+
+def editarCiudad():
+    os.system("cls")
+    _id = input("degite el Id de la ciudad que desea editar: ")
+    
+    daoCity=dao.DaoCity(conex)
+    ciudad = daoCity.get_by_id(_id)
+    print(ciudad)
+
+    _name= input("digite el nombre de la ciudad: ")
+    _status= input("digite el estatus: ")
+    daoCity=dao.DaoCity(conex)
+    daoCity.update(_name, _status, _id)
+
+    b = daoCity.get_by_id(_id)
+    print(b)
+
+def mostrarCiudad():
+    os.system("cls")
+    daoCity=dao.DaoCity(conex)
+    cities = daoCity.get_all()
+    print("Lista de Ciudades: ")
+    for city in cities:
+        print(city)
+
+def eliminarCiudad():
+    _id= input("Digite el Id de la ciudad que desea borrar: ")
+    daoCity=dao.DaoCity(conex)
+    ciudad = daoCity.delete(_id)
+    print(ciudad)
+
+
+def buscarCiudadID():
+    _id= input("Digite el Id de la ciudad que desea mostrar: ")
+    daoCity=dao.DaoCity(conex)
+    ciudad = daoCity.get_by_id(_id)
+    print(ciudad)
+
+def buscarCiudadname():
+    name = input("Digite el nombre de la ciudad que desea mostrar: ")
+    daoCity=dao.DaoCity(conex)
+    ciudad = daoCity.get_by_name(name)
+    print(ciudad)
+
+#CRUD Empleados
+def insertarEmpleado():
+    print("***Digite los datos del Empleado***")
+    first_name = input("Nombre: ")
+    last_name = input("Apellido: ")
+    city = input("Id ciudad: ")
+    job = input("Id Puesto: ")
+    salary = input("Salario: ")
+    Employee = clases.Employee(first_name, last_name, city, job, salary, 1)
+    daoEmployee = dao.DaoEmployees(conex)
+    #insertar
+    daoEmployee.insert(Employee)
+
+def mostrarEmpleado():
+    os.system("cls")
+    daoEmployee=dao.DaoEmployees(conex)
+    employees = daoEmployee.get_all()
+    print("Lista de Empleados: ")
+    for employee in employees:
+        print(employee)
+
+def editarEmpleado():
+    os.system("cls")
+    _id= input("digite el Id del empleado que desea editar: ")
+    daoEmployee = dao.DaoEmployees(conex)
+    a = daoEmployee.get_by_id(_id)
+    print("Empleado seleccionado: ")
+    print(a)
+
+    first_name = input("Nombre: ")
+    last_name = input("Apellido: ")
+    city = input("Id ciudad: ")
+    job = input("Id Puesto: ")
+    salary = input("Salario: ")
+    status = input("status: ")
+    daoEmployee=dao.DaoEmployees(conex)
+    Employee = clases.Employee(first_name, last_name, city, job, salary, status)
+    daoEmployee.update(Employee, _id)
+
+    b = daoEmployee.get_by_id(_id)
+    print("Empleado editado exitosamente: ")
+    print(b)
+
+
+def eliminarEmpleado():
+    _id= input("Digite el Id del empleado que desea borrar: ")
+    
+    daoEmployeee = dao.DaoEmployees(conex)
+    a = daoEmployeee.get_by_id(_id)
+    print("Empleado seleccionado: ")
+    print(a)
+    
+    Employee = daoEmployeee.delete(_id)
+    print(Employee)
+
+def buscarEmpleado():
+    _id= input("Digite el Id de la ciudad que desea mostrar: ")
+    daoEmployee = dao.DaoEmployees(conex)
+    Employee = daoEmployee.get_by_id(_id)
+    print(Employee)
+
+
+#CRUD Jobs
+def insertarPuesto():
+    name = input("Nombre del puesto: ")
+    job = clases.Job(name,1)
+    daoJob = dao.DaoJobs(conex)
+    #insertar
+    daoJob.insert(job)
+
+def mostrarPuestos():
+    os.system("cls")
+    daojob = dao.DaoJobs(conex)
+    jobs = daojob.get_all()
+    print("Lista de Puestos: ")
+    for job in jobs:
+        print(job)
+
+def eliminarPuesto():
+    _id= input("Digite el Id del puesto que desea borrar: ")
+    daoJob = dao.DaoJobs(conex)
+    a = daoJob.get_by_id(_id)
+    print(a)
+    Job = daoJob.delete(_id)
+    print(Job)
+
+
+def editarPuesto():
+    os.system("cls")
+    _id = input("Digite el Id del puesto que desea editar: ")
+    
+    daoJob=dao.DaoJobs(conex)
+    a = daoJob.get_by_id(_id)
+    print(a)
+
+    _name= input("Digite el nombre del puesto: ")
+    _status= input("Digite el estatus: ")
+    daoJob=dao.DaoJobs(conex)
+    Job = clases.Job(_name, _status)
+    daoJob.update(Job, _id)
+
+    b = daoJob.get_by_id(_id)
+    print(b)
+
+
+def buscarPuesto():
+    _id= input("Digite el Id del puesto que desea mostrar: ")
+    daoJob = dao.DaoJobs(conex)
+    Job = daoJob.get_by_id(_id)
+    print(Job)
+
+
+#Menus aplicados
+def menu():
+    os.system("cls")
+    print("***Menu Principal***")
+    print("1. Menu ciudades")
+    print("2. Menu Empleados")
+    print("3. Menu Puestos")
+    print("4. Salir")
+
+def menuCiudad():
+    print("***Menu Ciudad***")
+    print("1. Ingresar ciudad")
+    print("2. Editar ciudad")
+    print("3. Mostrar ciudades")
+    print("4. Eliminar ciudad")
+    print("5. Buscar ciudad")
+    print("6. Salir")
+
+def menuJobs():
+    print("***Menu Puestos***")
+    print("1. Ingresar Puesto")
+    print("2. Editar Puesto")
+    print("3. Mostrar Puestos")
+    print("4. Eliminar Puesto")
+    print("5. Buscar Puesto")
+    print("6. Salir")
+
+def menuEmployees():
+    print("***Menu Empleados***")
+    print("1. Ingresar empleado")
+    print("2. Editar empleado")
+    print("3. Mostrar empleados")
+    print("4. Eliminar empleado")
+    print("5. Buscar empleado")
+    print("6. Salir")
+
+
+#Main de pantallas
+def main():
+    opcion = ""
+
+    while opcion != "4":
+        menu()
+        opcion = input("Dime tu opcion: ")
+
+        if opcion == "1":
+            mainCiudad()
+            os.system("pause")
+        elif opcion == "2":
+            mainEmployees()
+            os.system("pause")
+        elif opcion == "3":
+            mainJobs()
+            os.system("pause")
+        elif opcion == "":
+            menu()
+
+def mainCiudad():
+    opcion = ""
+
+    while opcion != "6":
+        os.system("cls")
+        menuCiudad()
+        opcion = input("Dime tu opcion: ")
+
+        if opcion == "1":
+            insertarCiudad()
+            os.system("pause")
+        elif opcion == "2":
+            editarCiudad()
+            os.system("pause")
+        elif opcion == "3":
+            mostrarCiudad()
+            os.system("pause")
+        elif opcion == "4":
+            eliminarCiudad()
+            os.system("pause")
+        elif opcion == "5":
+            buscarCiudadname()
+            os.system("pause")
+        elif opcion == "":
+            menuCiudad()
+
+def mainJobs():
+    opcion = ""
+
+    while opcion != "6":
+        os.system("cls")
+        menuJobs()
+        opcion = input("Dime tu opcion: ")
+
+        if opcion == "1":
+            insertarPuesto()
+            os.system("pause")
+        elif opcion == "2":
+            editarPuesto()
+            os.system("pause")
+        elif opcion == "3":
+            mostrarPuestos()
+            os.system("pause")
+        elif opcion == "4":
+            eliminarPuesto()
+            os.system("pause")
+        elif opcion == "5":
+            buscarPuesto()
+            os.system("pause")
+        elif opcion == "":
+            menuJobs()
+
+def mainEmployees():
+    opcion = ""
+
+    while opcion != "6":
+        os.system("cls")
+        menuEmployees()
+        opcion = input("Dime tu opcion: ")
+
+        if opcion == "1":
+            insertarEmpleado()
+            os.system("pause")
+        elif opcion == "2":
+            editarEmpleado()
+            os.system("pause")
+        elif opcion == "3":
+            mostrarEmpleado()
+            os.system("pause")
+        elif opcion == "4":
+            eliminarEmpleado()
+            os.system("pause")
+        elif opcion == "5":
+            buscarEmpleado()
+            os.system("pause")
+        elif opcion == "":
+            menuEmployees()
+        
+
+
+main()
